abbcfc4ee0a0c3f0db91165c9e59407479aca647
Minry3 <noemiechhun@gmail.com>
2026-05-11 13:53:36 +0200

    ajout d'un import dans la classe de tests sans mock

--- src/test/java/com/example/demo/web/WebTestsSansMock.java
+++ src/test/java/com/example/demo/web/WebTestsSansMock.java
@@ -13,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.annotation.DirtiesContext;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
